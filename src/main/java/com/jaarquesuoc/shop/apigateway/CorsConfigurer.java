8e1a18733c66f4a4880dea496e5335d4df6edbcf
Jordi <jaarques@uoc.edu>
2019-05-10 01:02:01 +0200

    Enabling CORS from frontend app

---
+++ src/main/java/com/jaarquesuoc/shop/apigateway/CorsConfigurer.java
@@ -0,0 +1,16 @@
+package com.jaarquesuoc.shop.apigateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+    private static final String EDGE_HOSTNAME = "jaarques-uoc.github.io";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(EDGE_HOSTNAME);
+    }
+}
